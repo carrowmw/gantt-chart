a057d2fc09998447513b0e5b5161c09af1eb89ff
Administrator <administrator@CMDadmins-MBP-2.neotis.local>
2024-06-19 19:18:37 +0100

    updates to main file

--- app/serve_app.py
+++ app/serve_app.py
@@ -5,94 +5,97 @@ import dash_bootstrap_components as dbc
 from src.model.gantt_chart import GanttChart
 
 
-app = dash.Dash(__name__)
+def serve_app():
+    app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-gantt_chart = GanttChart()
+    gantt_chart = GanttChart()
 
-left_config_column = dbc.Col(
-    children=[
-        dcc.Dropdown(
-            id="chart-dropdown",
-            options=[
-                {"label": "Gantt Chart with Slippage", "value": "slippage"},
-                {"label": "Updated Gantt Chart", "value": "updated"},
-                {"label": "Baseline Gantt Chart", "value": "baseline"},
-            ],
-            value="slippage",
-        ),
-    ],
-    width=6,
-    align="center",
-    class_name="column-element dash-dropdown",
-)
+    left_config_column = dbc.Col(
+        children=[
+            dcc.Dropdown(
+                id="chart-dropdown",
+                options=[
+                    {"label": "Gantt Chart with Slippage", "value": "slippage"},
+                    {"label": "Updated Gantt Chart", "value": "updated"},
+                    {"label": "Baseline Gantt Chart", "value": "baseline"},
+                ],
+                value="slippage",
+            ),
+        ],
+        width=6,
+        align="center",
+        class_name="column-element dash-dropdown",
+    )
 
-right_config_column = dbc.Col(
-    children=[
-        dcc.Checklist(
-            id="interruptions-checklist",
-            options=[
-                {
-                    "label": "Include Interruptions",
-                    "value": "include_interruptions",
-                },
-            ],
-            value=["include_interruptions", "include_milestones"],
-        ),
-        dcc.Checklist(
-            id="milestones-checklist",
-            options=[
-                {"label": "Include Milestones", "value": "include_milestones"},
-            ],
-            value=["include_milestones"],
-        ),
-    ],
-    width=6,
-    align="center",
-    class_name="column-element dash-checklist",
-)
+    right_config_column = dbc.Col(
+        children=[
+            dcc.Checklist(
+                id="interruptions-checklist",
+                options=[
+                    {
+                        "label": "Include Interruptions",
+                        "value": "include_interruptions",
+                    },
+                ],
+                value=["include_interruptions", "include_milestones"],
+            ),
+            dcc.Checklist(
+                id="milestones-checklist",
+                options=[
+                    {"label": "Include Milestones", "value": "include_milestones"},
+                ],
+                value=["include_milestones"],
+            ),
+        ],
+        width=6,
+        align="center",
+        class_name="column-element dash-checklist",
+    )
 
-top_row = dbc.Row(
-    children=[
-        left_config_column,
-        right_config_column,
-    ],
-    class_name="row-element ",
-    style={"width": "100%"},
-)
+    top_row = dbc.Row(
+        children=[
+            left_config_column,
+            right_config_column,
+        ],
+        class_name="row-element ",
+        style={"width": "100%"},
+    )
 
-bottom_row = dbc.Row(children=[dcc.Graph(id="gantt-chart")], class_name="row-element")
+    bottom_row = dbc.Row(
+        children=[dcc.Graph(id="gantt-chart")], class_name="row-element"
+    )
 
+    app.layout = html.Div(children=[top_row, bottom_row])
 
-app.layout = html.Div(children=[top_row, bottom_row])
+    @app.callback(
+        Output("gantt-chart", "figure"),
+        Input("chart-dropdown", "value"),
+        Input("interruptions-checklist", "value"),
+        Input("milestones-checklist", "value"),
+    )
+    def update_chart(selected_chart, interruptions_checklist, milestones_checklist):
+        include_interruptions = "include_interruptions" in interruptions_checklist
+        include_milestones = "include_milestones" in milestones_checklist
 
+        if selected_chart == "slippage":
+            fig = gantt_chart.create_gantt_chart_with_slippage(
+                include_interruptions, include_milestones
+            )
+        elif selected_chart == "updated":
+            fig = gantt_chart.create_updated_gantt_chart(
+                include_interruptions, include_milestones
+            )
+        elif selected_chart == "baseline":
+            fig = gantt_chart.create_baseline_gantt_chart(
+                include_interruptions, include_milestones
+            )
+        else:
+            fig = {}
 
-@app.callback(
-    Output("gantt-chart", "figure"),
-    Input("chart-dropdown", "value"),
-    Input("interruptions-checklist", "value"),
-    Input("milestones-checklist", "value"),
-)
-def update_chart(selected_chart, interruptions_checklist, milestones_checklist):
-    include_interruptions = "include_interruptions" in interruptions_checklist
-    include_milestones = "include_milestones" in milestones_checklist
+        return fig
 
-    if selected_chart == "slippage":
-        fig = gantt_chart.create_gantt_chart_with_slippage(
-            include_interruptions, include_milestones
-        )
-    elif selected_chart == "updated":
-        fig = gantt_chart.create_updated_gantt_chart(
-            include_interruptions, include_milestones
-        )
-    elif selected_chart == "baseline":
-        fig = gantt_chart.create_baseline_gantt_chart(
-            include_interruptions, include_milestones
-        )
-    else:
-        fig = {}
-
-    return fig
+    return app
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    serve_app()
--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-
-# Checks to see if the src package is a package and if it is, it will print out the submodules.
-import pkgutil
-import src
-
-package = src
-for importer, modname, ispkg in pkgutil.iter_modules(package.__path__):
-    print(f"Found submodule {modname} (is a package: {ispkg})")
-
-# This file is the entry point of the application. It creates the app and runs it.
-from src.app.serve_app import create_app
+from app.serve_app import serve_app
 
 if __name__ == "__main__":
-    create_app().run_server(debug=True)
+    app = serve_app()
+    app.run_server(debug=True)
